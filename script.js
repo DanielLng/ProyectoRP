a034cd95157d9a69d0ce7be62a135e9466fa51db
DanielLng <99903015+DanielLng@users.noreply.github.com>
2022-12-15 12:53:03 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
+
+
 function initThemeSelector(){
     const themeSelect = document.getElementById("themeSelect");
     const themeSylesheetLink = document.getElementById("themeSylesheetLink");
     const currentTheme = localStorage.getItem("theme") || "default";
 
     function activateTheme(themeName){
-      themeSylesheetLink.setAttribute("href", `/temas/${themeName}.css`);
+      themeSylesheetLink.setAttribute("href", `./temas/${themeName}.css`);
     }
 
     themeSelect.addEventListener("change", () => {
